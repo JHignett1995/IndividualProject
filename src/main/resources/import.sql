c5f4f794f879881c9cffb6a8579b74436bf30282
Jordan <jorda.hignett1@googlemail.com>
2019-03-06 17:01:48 +0000

    adv

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO Player (email, name, title ,password, winCount, loseCount,count7Ball,rivalID,isAdmin) VALUES ('1@qa.com', 'Jordan Hignett','Champ','Password1',15,4,0,'chester@qa.com',true)
+INSERT INTO Player (email, name, title ,password, winCount, loseCount,count7Ball,rivalID,isAdmin) VALUES ('1@qa.com', 'Jordan','Champ','Password1',15,4,0,'chester@qa.com',true)
